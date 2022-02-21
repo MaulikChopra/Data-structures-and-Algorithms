a4a56fb322ba88db55f5b46383d4d3216ff7f524
maulikchopra <maulikchopra10@gmail.com>
2022-02-21 21:22:32 +0530

    binary search recursive done

--- Binary Search/binarySearch.py
+++ Binary Search/binarySearch.py
@@ -1,72 +1,27 @@
-import time
+# Binary Search recursive.
+# O(log(n)) - time complexity.
+
+def recursive(array, value):
+    return bs(array, value, 0, len(array))
 
 
-def binary_search(array, value):
-    length = len(array)
-
-    def bs(array, length, value):
-        if length != 0 and value <= array[length-1] and value >= array[0]:
-            if value == array[length//2]:
-                return True
-            elif value > array[length//2]:
-                # print(array[length//2:])
-                return binary_search(array[length//2:], value)
-            elif value < array[length//2]:
-                # print(array[:length//2])
-                return binary_search(array[:length//2], value)
-        else:
-            return False
-    return bs(array, length, value)
-
-
-def binary_search_length(array, length, value):
-    if length != 0 and value <= array[length-1] and value >= array[0]:
-        if value == array[length//2]:
-            return True
-        elif value > array[length//2]:
-            print(array[length//2:])
-            return binary_search_length(array[length//2:], length//2, value)
-        elif value < array[length//2]:
-            print(array[:length//2])
-            return binary_search_length(array[:length//2], length//2, value)
-    else:
+def bs(array, value, start, end):
+    middle = (start + end) // 2
+    if start == end:
         return False
+    elif value > array[middle]:  # right
+        return bs(array, value, middle + 1, len(array))
+    elif value < array[middle]:  # left
+        return bs(array, value, start, middle)
+    return True, middle
 
 
-def linear_search(array, value):
-    for i in array:
-        if i == value:
-            return True
-    return False
+def iterative():
+    pass
 
 
 # DRIVER CODE
-arr = [1, 2, 3, 4, 6, 7, 8, 9]
-val = 50
-# BINARY SEARCH NO LENGTH BEST ONE
-start = time.perf_counter()
-if binary_search_length(arr, len(arr), val):
-    print("found it")
-else:
-    print("not found")
-end = time.perf_counter()
-print(f"code ran in {end-start:0.4f} seconds.")
-
-# BINARY SEARCH GIVE LENGTH
-start = time.perf_counter()
-if binary_search(arr, val):
-    print("found it")
-else:
-    print("not found")
-end = time.perf_counter()
-print(f"code ran in {end-start:0.4f} seconds.")
-
-
-# LINEAR NOOB SEARCH
-start = time.perf_counter()
-if linear_search(arr, val):
-    print("found it")
-else:
-    print("not found")
-end = time.perf_counter()
-print(f"code ran in {end-start:0.4f} seconds.")
+if __name__ == "__main__":
+    arr = [i for i in range(0, 10)]
+    find = 8
+    print(recursive(arr, find))
