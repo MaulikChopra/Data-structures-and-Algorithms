27c02166b5bf647a9ad305f8612a225dd587cfa3
maulik <maulikchopra10@gmail.com>
2022-02-26 14:56:55 +0530

    ubuntu first edit comit proper logic

--- Binary Search/BinarySearch.py
+++ Binary Search/BinarySearch.py
@@ -5,13 +5,17 @@ def recursive(array, value):
     return bs(array, value, 0, len(array))
 
 
-def bs(array, value, start, end):
+def bs(array, value, left_iterator, right_iterator):
+    start = left_iterator
+    end = right_iterator
     middle = (start + end) // 2
     if start == end:
         return False
     elif value > array[middle]:  # right
-        return bs(array, value, middle + 1, len(array))
+        print(start, middle, end)
+        return bs(array, value, middle + 1, end)
     elif value < array[middle]:  # left
+        print(start, middle, end)
         return bs(array, value, start, middle)
     return True, middle
 
@@ -22,6 +26,7 @@ def iterative():
 
 # DRIVER CODE
 if __name__ == "__main__":
-    arr = [i for i in range(0, 10)]
+    arr = [i for i in range(1, 51)]
     find = 8
+    print("(bool, index)")
     print(recursive(arr, find))
--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Data-structures-and-Algorithms
-## Hi I am Maulik!
-## This is my project where I practice Data Structures and Algorithms in PYTHON
+
+### Hi I am Maulik!
+
+### This is my project where I practice Data Structures and Algorithms in PYTHON
+
+## PROGRESS CHECKER.
+
+<p>
+<ul>
+    <li>
+    <b>Search Algorithm</b>
+        <ol>
+            <li>Binary Search Recursive</li>
+        </ol>
+    </li>
+    <li>
+    <b>Linked List</b>
+        <ol>
+            <li>In progress</li>
+        </ol>
+    </li>
+</ul>
+</p>
